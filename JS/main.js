f7710807b0894df4bd765fefe21bb081fb387a22
Anshul Mokhale <anshulmokhale0712@gmail.com>
2023-09-07 07:53:32 +0530

    Sixth Commit

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
+    $.get("http://localhost/ChatApp/PHP_BACKEND/chat.php", function (data) {
+        if (data.email) {
+            console.log("Logged in user: " + data.email);
+        }
+        for (var i = 0; i < data.messages.length; i++) {
+            var senderName = data.name[i];
+            var message = data.messages[i];
+            var time = data.time[i];
 
+            console.log("Sender: " + senderName);
+            console.log("Message: " + message);
+            console.log("Time: " + time);
+        }
+    });
 });
--- PHP_BACKEND/chat.php
+++ PHP_BACKEND/chat.php
@@ -1,13 +1,22 @@
 <?php
 
 session_start();
+include "./connect.php";
 
-header("Content-Type:application/json");
+header("Content-Type: application/json");
 
-if($_SERVER["REQUEST_METHOD"]=="GET")
-{
-    $response = array(
-        "email" => $_SESSION["userid"]
-    );
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $response = array();
+    if (isset($_SESSION["userid"])) {
+        $response["email"] = $_SESSION["userid"];
+    }
+    $query = $pdo->prepare("SELECT * FROM messages");
+    $query->execute();
+    $messages = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $response["name"] =  array_column($messages, "sender_name");
+    $response["messages"] =  array_column($messages, "msg");
+    $response["time"] =  array_column($messages, "time");
+
     echo json_encode($response);
 }
